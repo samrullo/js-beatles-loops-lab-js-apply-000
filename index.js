3c8e74e48a4643713b86204fb7677c2a95ec297f
Subkhon Amrulloev <subkhon.amrulloev@blackrock.com>
2019-03-21 05:08:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var mus_inst_arr=[]
+  for(var i=0;i<musicians.length;i++){
+    mus_inst_arr.push(musicians[i]+" plays "+instruments[i]);
+  }
+  return mus_inst_arr;
+}
+
+function johnLennonFacts(jl_facts){
+  
+}
